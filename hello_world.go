3c114abd35f3899d8b883836d8aacbeacfcb8cc4
Shourawandy <soroar42@gmail.com>
2025-10-04 09:45:36 +0600

    add new file and organize with comment

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,9 @@
+// Package declaration - every Go program starts with a package.
 package main
 
-import "fmt"
+import "fmt" // Here, we import the "fmt" package for formatted I/O (input/output).
 
 func main() {
-	fmt.Println("hello world! ")
+
+	fmt.Println("hello world!") // Print a message to the console.
 }
---
+++ print_variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10 // Declare and initialize variable a with value 10
+
+	fmt.Println(a) // Print the value of a to the console
+}
